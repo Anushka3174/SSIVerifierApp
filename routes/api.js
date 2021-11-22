7aefb4bf98f6a3455124ea5bc8a4bb524840c153
Anushka Subedi <anushka.subedi3174@gmail.com>
2021-11-22 18:06:30 +0100

    first commit

---
+++ routes/api.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const { CredentialsServiceClient, Credentials } = require("@trinsic/service-clients");
+require('dotenv').config();
+
+const client = new CredentialsServiceClient(
+    new Credentials(process.env.ACCESSTOK),
+    { noRetryPolicy: true });
+
+router.post('/issue', cors(), async function (req, res) {
+  let params = {
+    definitionId: process.env.CRED_DEF_ID,
+    automaticIssuance: true,
+    credentialValues: {
+      "Full Name": req.body.name,
+      "Address": req.body.address,
+      "Passport Number": req.body.passportNumber,
+      "Date of Birth": req.body.dob,
+    }
+  }
+    console.log('\x1b[36m%s\x1b[0m', 'I am cyan', params);
+
+    let result = await client.createCredential(params);
+
+  res.status(200).send({ offerData: result.offerData, offerUrl: result.offerUrl });
+});
+
+router.post('/verify', cors(), async function (req, res) {
+  console.log("This is policy ID",process.env.POLICY_ID)
+  let verification = await client.createVerificationFromPolicy(process.env.POLICY_ID);
+
+  res.status(200).send({
+    verificationRequestData: verification.verificationRequestData,
+    verificationRequestUrl: verification.verificationRequestUrl,
+    verificationId: verification.verificationId
+  });
+});
+
+router.get('/checkVerification', cors(), async function (req, res) {
+  let verificationId = req.query.verificationId;
+  let verification = await client.getVerification(verificationId);
+
+  res.status(200).send({
+    verification: verification
+  });
+});
+
+module.exports = router;
